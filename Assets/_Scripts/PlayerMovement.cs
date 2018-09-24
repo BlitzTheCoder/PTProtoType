c2efd5c97b81aa6e3297b1d10155a32e50c7bb11
Unknown <brown.r1992@gmail.com>
2018-09-24 16:13:10 -0400

    Add Player Prefab and Movement Script
    + PlayerMovement.CS
    + Player Prefab
       - Capsule
       - Camera
    + Basic Forward/Backward movement
    + Settable walk speed
    + View Bob speed (not implemented)

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Camera cam;
+    public GameObject playerBody;
+
+    public float walkSpeed = 1;
+    public float headBob = 1;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
+        {
+            Walk();
+        }
+
+    }
+
+    void Walk()
+    {
+        float f = Input.GetAxisRaw("Vertical") * Time.deltaTime;
+        float s = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+
+        transform.position += ((transform.forward * f) + (transform.right * s)) * walkSpeed;
+
+    }
+
+
+}
