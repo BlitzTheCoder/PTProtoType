263288e1ac68603943b858769065f59cdccf05ab
Unknown <brown.r1992@gmail.com>
2018-09-24 20:54:41 -0400

    Add Joystick Movements
    + Joystick inputs in Input Manager
        - Left Joystick moves Player
        - Right Joystick moves Camera

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     public bool invertYAxis = true;
 
     public float walkSpeed = 1;
+    public float cameraSensitivity = 1;
     public float headBob = 1;
 
 
@@ -25,17 +26,17 @@ public class PlayerMovement : MonoBehaviour
     {
 
         LookAround();
-        if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
-        {
-            Walk();
-        }
+
+        Walk();
+
+
 
     }
 
     void Walk()
     {
-        float f = Input.GetAxisRaw("Vertical") * Time.deltaTime;
-        float s = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        float f = Input.GetAxis("Vertical") * Time.deltaTime;
+        float s = Input.GetAxis("Horizontal") * Time.deltaTime;
 
         transform.position += ((transform.forward * f) + (transform.right * s)) * walkSpeed;
 
@@ -43,14 +44,17 @@ public class PlayerMovement : MonoBehaviour
 
     void LookAround()
     {
-        float lX = (invertXAxis) ? Input.GetAxisRaw("Mouse X") * -1: Input.GetAxisRaw("Mouse X") * 1;
-        float lY = (invertYAxis) ? Input.GetAxisRaw("Mouse Y") * -1: Input.GetAxisRaw("Mouse Y") * 1;
+        float lX = Input.GetAxis("Camera X") * (invertXAxis ? -1 : 1) * cameraSensitivity;
+        float lY = Input.GetAxis("Camera Y") * (invertYAxis ? -1 : 1) * cameraSensitivity;
+
+        float newAngle = cam.transform.localEulerAngles.x + lY;
+        if (newAngle > 80 && newAngle < 280) return;
 
         transform.Rotate(0, lX, 0);
 
         cam.transform.Rotate(lY, 0, 0);
 
-        Mathf.Clamp(transform.rotation.y, -350, 0);
+
 
     }
 
