5803927cdd18c201510bcc08cf9d739338907fbf
Blitz <brown.r1992@gmail.com>
2018-09-27 17:24:13 -0400

    Comment Code
    Add comments to Player Movement Code

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This Script is applied to a player object
+/// and a Camera must be set.
+/// </summary>
+
 [RequireComponent(typeof(Camera))]
 public class PlayerMovement : MonoBehaviour
 {
@@ -33,22 +38,36 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+
+    /// <summary>
+    /// Using any controls that invoke the "Vertical" and "Horizontal" inputs
+    /// the player can walk forwards or backwards and left or right.
+    /// </summary>
     void Walk()
     {
-        float f = Input.GetAxis("Vertical") * Time.deltaTime;
+        //Set values to somewhere between -1 and 1 depending on inputs
+        float f = Input.GetAxis("Vertical") * Time.deltaTime; 
         float s = Input.GetAxis("Horizontal") * Time.deltaTime;
 
+        //Move the player either positively or negatively along their respective axis depending on input at the specified walking speed.
         transform.position += ((transform.forward * f) + (transform.right * s)) * walkSpeed;
 
     }
 
+
+    /// <summary>
+    /// Using any controls, in this case the mouse or a DualShock 4 controller,
+    /// the player can freely look around, with the Cameras pitch locked to look
+    /// between either straight up or straight down.
+    /// </summary>
     void LookAround()
     {
+        //Set values to somewhere between -1 and 1 depending on inputs and inverts that number depending on invertAxis booleans
         float lX = Input.GetAxis("Camera X") * (invertXAxis ? -1 : 1) * cameraSensitivity;
         float lY = Input.GetAxis("Camera Y") * (invertYAxis ? -1 : 1) * cameraSensitivity;
 
-        float newAngle = cam.transform.localEulerAngles.x + lY;
-        if (newAngle > 80 && newAngle < 280) return;
+        float newAngle = cam.transform.localEulerAngles.x + lY; //Grab current camera Euler Angle to use in following if statement.
+        if (newAngle > 80 && newAngle < 280) return; //Locks camera range to straight up and straight down
 
         transform.Rotate(0, lX, 0);
 
