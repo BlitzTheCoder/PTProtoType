096bf81dc312aa0f2e3bdf16e025723f8cbaa9da
Unknown <brown.r1992@gmail.com>
2018-10-16 21:42:08 -0400

    Add RayCast
    +RayCast Script
    +"Interactable" Tag

---
+++ Assets/_Scripts/RayCast.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour {
+
+	
+	void Update ()
+    {
+        CastRay();
+    }
+
+    /// <summary>
+    /// Casts out a ray from the Player Camera position and returns a hit if the object has the "Interactable" tag.
+    /// </summary>
+    void CastRay()
+    {
+        
+        RaycastHit hit;
+        
+        // The distance at which the other object is when hit.
+        float theDistance;
+
+        // Sets the forward of the Ray at a specified distance.
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 1;
+
+        // Sets the Ray as red in the editor during play.
+        Debug.DrawRay(transform.position, forward, Color.red);
+
+        //If the Raycast hits an object...
+        if (Physics.Raycast(transform.position, (forward), out hit))
+        {
+            //If that object has the "Interactable" Tag...
+            if (hit.collider.gameObject.tag == "Interactable")
+            {
+                theDistance = hit.distance;
+
+                print(theDistance + " " + hit.collider.gameObject.name);
+            }
+        }
+    }
+}
